70e7a0a4dd2d6650d5b1109e7ab973be671f29e7
doorahmie <doorahmie@doorahmies-Mac-mini.local>
2024-06-09 17:42:43 +0900

    vuetify

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link :to="{ name: 'EventList' }">Events</router-link> |
-      <router-link :to="{ name: 'About' }">About</router-link>
-      |
+      <router-link :to="{ name: 'About' }">About</router-link> |
       <router-link :to="{ name: 'EventCreate' }">Create Event</router-link>
     </div>
     <router-view />
@@ -61,7 +60,7 @@ input {
   margin-bottom: 24px;
 }
 
-input[type='text'] {
+input[type="text"] {
   padding: 0px 10px;
 }
 
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './stores'
-
-createApp(App).use(store).use(router).mount('#app')
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./stores";
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
+/* https://v15.vuetifyjs.com/ko/getting-started/quick-start/ */
+createApp(App).use(store).use(router).use(Vuetify).mount("#app");
