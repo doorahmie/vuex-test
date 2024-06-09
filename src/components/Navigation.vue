aeb8275ad47497bbd764927ce52694f6229d6ea6
doorahmie <doorahmie@doorahmies-Mac-mini.local>
2024-06-09 20:04:35 +0900

    prettier적용

---
+++ src/components/Navigation.vue
@@ -0,0 +1,7 @@
+<v-app-bar :elevation="2" rounded>
+  <template v-slot:prepend>
+    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+  </template>
+
+  <v-app-bar-title>Application Bar</v-app-bar-title>
+</v-app-bar>
